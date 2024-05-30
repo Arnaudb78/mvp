2112529ae2d0677c3ad349271bee85e92ece9528
Arnaud Beaulieu <arnaudbeaulieu@MacBook-Pro-de-Arnaud.local>
2024-05-30 16:48:00 +0200

    DONE: clean files & folders

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-5xl font-bold">Hello, Studio miam!</h1>
-      <button className="bg-red-100 p-2 rounded-lg hover:bg-red-400 ">Click</button>
+      <h1 className="text-3xl font-extrabold">HELLO STUDIO MIAM!</h1>
     </main>
   );
 }
